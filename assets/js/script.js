aa0229bc3f012870550f1feebb4d5d002ce1253a
ThuyTiencodelight <tienht@codelight.co>
2022-11-09 15:02:09 +0700

    change text, img, link and add animation for map

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,14 +6,46 @@ $(function () {
     });
 
     function setFixedHeader() {
-        if (window.scrollY > 50 && $(window).width() > 767) {
-            $('#header').addClass('sticky');
-        } else {
-            $('#header').removeClass('sticky');
+        if ($(window).width() >= 768) {
+            if (window.scrollY > 123) {
+                $('#header').addClass('sticky');
+            } else {
+                $('#header').removeClass('sticky');
+            }
+        }
+        else{
+            $('#header').addClass('sticky cl-dark');
         }
     }
-    
     window.setTimeout(() => {
         $('#CLLoading').fadeOut();
     }, 400)
 })
+$(document).ready(function(){
+    new WOW().init();
+})
+document.getElementById("mc-embedded-subscribe").addEventListener("click", function () {
+    var email = document.getElementById("mce-EMAIL").value;
+    if (validateEmail(email)) {
+        $.ajax({
+            type: 'post',
+            url: 'https://subscribe.metabase.build',
+            data: JSON.stringify({
+                email: email,
+                project: 'Codelight'
+            }),
+            contentType: "application/json; charset=utf-8",
+            success: function (res) {
+                document.getElementById("message").innerHTML = "Your subscription has been successful.";
+                bootstrap.Modal.getOrCreateInstance(document.getElementById('SubscribeModal')).show();
+            },
+            error: function (err) {
+                document.getElementById("message").innerHTML = "Your subscription could not be saved. Please try again.";
+                bootstrap.Modal.getOrCreateInstance(document.getElementById('SubscribeModal')).show();
+            }
+        });
+    }
+});
+function validateEmail(email) {
+    return /^\w+([+\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+}
